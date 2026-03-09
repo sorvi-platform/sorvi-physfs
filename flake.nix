f4dfce7c7e767d374196cc39ea72b1bec4c529d1
Jari Vetoniemi <jari.vetoniemi@cloudef.pw>
2026-03-09 19:00:32 +0900

    flake: update to 0.15.2

--- flake.nix
+++ flake.nix
@@ -11,20 +11,7 @@
       # Zig flake helper
       # Check the flake.nix in zig2nix project for more options:
       # <https://github.com/Cloudef/zig2nix/blob/master/flake.nix>
-      env = zig2nix.outputs.zig-env.${system} { zig = zig2nix.outputs.packages.${system}.zig-0_15_1; };
-
-      frontend_runtime_deps = with env.pkgs; [
-        vulkan-loader
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXrandr
-        libxkbcommon
-        wayland
-        alsa-lib
-        libGL
-      ];
-
+      env = zig2nix.outputs.zig-env.${system} { zig = zig2nix.outputs.packages.${system}.zig-0_15_2; };
     in {
       # nix run .
       apps.default = env.app [] "zig build run -- \"$@\"";
@@ -33,11 +20,6 @@
       apps.build = env.app [] "zig build \"$@\"";
 
       # nix develop
-      devShells.default = env.mkShell {
-        nativeBuildInputs = frontend_runtime_deps;
-        shellHook = ''
-          export VK_LAYER_PATH="${env.pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d"
-          '';
-      };
+      devShells.default = env.mkShell {};
     }));
 }
